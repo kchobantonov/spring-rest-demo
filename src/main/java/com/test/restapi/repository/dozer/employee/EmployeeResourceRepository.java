7000b1af2d6cd3639f70fc8a0d49a1d283c94b06
Krasimir Chobantonov <kchobantonov@gmail.com>
2020-02-20 02:41:13 -0500

    update demo with query methods

--- src/main/java/com/test/restapi/repository/dozer/employee/EmployeeResourceRepository.java
+++ src/main/java/com/test/restapi/repository/dozer/employee/EmployeeResourceRepository.java
@@ -1,7 +1,13 @@
 package com.test.restapi.repository.dozer.employee;
 
+import java.util.List;
+import java.util.stream.Stream;
+
+import javax.transaction.Transactional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.dozer.repository.DozerRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,5 +19,25 @@ import com.test.restapi.entity.dozer.employee.EmployeeResource;
 public interface EmployeeResourceRepository extends DozerRepository<EmployeeResource, Integer> {
 
 	@Query("SELECT e FROM Employee e WHERE e.firstName LIKE :firstName")
-	Page<EmployeeResource> findByFirstName(@Param("firstName") String firstName, Pageable pageable);
+	Page<EmployeeResource> findByFirstNameAndReturnPage(@Param("firstName") String firstName, Pageable pageable);
+
+	@Query("SELECT e FROM Employee e WHERE e.firstName LIKE :firstName")
+	List<EmployeeResource> findByFirstNameAndReturnList(@Param("firstName") String firstName, Pageable pageable);
+
+	@Query("SELECT e FROM Employee e WHERE e.firstName LIKE :firstName")
+	Slice<EmployeeResource> findByFirstNameAndReturnSlice(@Param("firstName") String firstName, Pageable pageable);
+
+	//TODO check the issue with transaction not been enabled
+	@Transactional
+	@Query("SELECT e FROM Employee e WHERE e.firstName LIKE :firstName")
+	Stream<EmployeeResource> findByFirstNameAndReturnStream(@Param("firstName") String firstName, Pageable pageable);
+
+	@Query("SELECT e FROM Employee e WHERE e.id = :id")
+	EmployeeResource findByCustomId(@Param("id") Integer id);
+
+	// projections
+
+	@Query("SELECT e.firstName FROM Employee e WHERE e.id = :id")
+	String findFirstNameByCustomId(@Param("id") Integer id);
+
 }
