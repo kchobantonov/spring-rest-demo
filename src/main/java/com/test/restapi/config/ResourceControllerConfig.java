2a587ed4e5b2c2f59da48ad9f1a6f986724f6cde
Krasimir Chobantonov <kchobantonov@gmail.com>
2020-02-26 01:06:10 -0500

    use more functional way to find the methods that allows also entity class and entity id classes to be more generic in the subclass

--- src/main/java/com/test/restapi/config/ResourceControllerConfig.java
+++ src/main/java/com/test/restapi/config/ResourceControllerConfig.java
@@ -1,14 +1,20 @@
 package com.test.restapi.config;
 
+import static java.util.Arrays.asList;
+import static org.springframework.data.util.Optionals.toStream;
+
 import java.lang.reflect.Method;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.BridgeMethodResolver;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.support.Repositories;
 import org.springframework.data.rest.core.Path;
@@ -20,8 +26,10 @@ import org.springframework.data.rest.webmvc.PersistentEntityResourceAssembler;
 import org.springframework.data.rest.webmvc.support.DelegatingHandlerMapping;
 import org.springframework.data.rest.webmvc.support.ETag;
 import org.springframework.data.util.ClassTypeInformation;
+import org.springframework.data.util.Optionals;
 import org.springframework.data.util.TypeInformation;
 import org.springframework.http.HttpHeaders;
+import org.springframework.util.ClassUtils;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
@@ -30,6 +38,12 @@ import com.test.restapi.controller.ResourceController;
 
 @Configuration
 public class ResourceControllerConfig {
+	private static final String GET_COLLECTION_RESOURCE = "getCollectionResource";
+	private static final String GET_ITEM_RESOURCE = "getItemResource";
+	private static final String POST_COLLECTION_RESOURCE = "postCollectionResource";
+	private static final String PUT_ITEM_RESOURCE = "putItemResource";
+	private static final String PATCH_ITEM_RESOURCE = "patchItemResource";
+	private static final String DELETE_ITEM_RESOURCE = "deleteItemResource";
 
 	@Autowired
 	public void setHandlerMapping(ListableBeanFactory factory, DelegatingHandlerMapping restHandlerMapping,
@@ -59,93 +73,163 @@ public class ResourceControllerConfig {
 
 			Path path = resourceMetadata.getPath();
 
-			Method getCollectionResourceMethod = ReflectionUtils.findMethod(handlerClass, "getCollectionResource",
-					new Class[] { Pageable.class, PersistentEntityResourceAssembler.class });
-
-			if (getCollectionResourceMethod != null
-					&& !getCollectionResourceMethod.getDeclaringClass().equals(ResourceController.class)) {
-
+			Optional<Method> getCollectionResourceMethod = selectMostSuitableGetCollectionResourceMethod(handlerClass);
+			if (isRedeclaredMethod(getCollectionResourceMethod)) {
 				RequestMappingInfo info = RequestMappingInfo
 						.paths(new Path(repositoryRestConfiguration.getBasePath().toString()).slash(path).toString())
 						.methods(RequestMethod.GET).build();
-				mapping.registerMapping(info, handler, getCollectionResourceMethod);
+				mapping.registerMapping(info, handler, getCollectionResourceMethod.get());
 			}
 
-			Method getItemResourceMethod = ReflectionUtils.findMethod(handlerClass, "getItemResource",
-					new Class[] { domainIdType, PersistentEntityResourceAssembler.class, HttpHeaders.class });
-
-			if (getItemResourceMethod != null
-					&& !getItemResourceMethod.getDeclaringClass().equals(ResourceController.class)) {
-
+			Optional<Method> getItemResourceMethod = selectMostSuitableGetItemResourceMethod(handlerClass,
+					domainIdType);
+			if (isRedeclaredMethod(getItemResourceMethod)) {
 				RequestMappingInfo info = RequestMappingInfo
 						.paths(new Path(repositoryRestConfiguration.getBasePath().toString()).slash(path.slash("{id"
 								+ (handler.getIdPathVariableRegex() != null ? ":" + handler.getIdPathVariableRegex()
 										: "")
 								+ "}")).toString())
 						.methods(RequestMethod.GET).build();
-				mapping.registerMapping(info, handler, getItemResourceMethod);
+				mapping.registerMapping(info, handler, getItemResourceMethod.get());
 			}
 
-			Method postCollectionResourceMethod = ReflectionUtils.findMethod(handlerClass, "postCollectionResource",
-					new Class[] { domainType, PersistentEntityResourceAssembler.class, String.class });
-
-			if (postCollectionResourceMethod != null
-					&& !postCollectionResourceMethod.getDeclaringClass().equals(ResourceController.class)) {
-
+			Optional<Method> postCollectionResourceMethod = selectMostSuitablePostCollectionResourceMethod(handlerClass,
+					domainType);
+			if (isRedeclaredMethod(postCollectionResourceMethod)) {
 				RequestMappingInfo info = RequestMappingInfo
 						.paths(new Path(repositoryRestConfiguration.getBasePath().toString()).slash(path).toString())
 						.methods(RequestMethod.POST).build();
-				mapping.registerMapping(info, handler, postCollectionResourceMethod);
+				mapping.registerMapping(info, handler, postCollectionResourceMethod.get());
 			}
-			
-			Method putItemResourceMethod = ReflectionUtils.findMethod(handlerClass, "putItemResource",
-					new Class[] { domainType, domainIdType, PersistentEntityResourceAssembler.class, ETag.class, String.class });
 
-			if (putItemResourceMethod != null
-					&& !putItemResourceMethod.getDeclaringClass().equals(ResourceController.class)) {
-
+			Optional<Method> putItemResourceMethod = selectMostSuitablePutItemResourceMethod(handlerClass, domainType,
+					domainIdType);
+			if (isRedeclaredMethod(putItemResourceMethod)) {
 				RequestMappingInfo info = RequestMappingInfo
 						.paths(new Path(repositoryRestConfiguration.getBasePath().toString()).slash(path.slash("{id"
 								+ (handler.getIdPathVariableRegex() != null ? ":" + handler.getIdPathVariableRegex()
 										: "")
-								+ "}")).toString())						
+								+ "}")).toString())
 						.methods(RequestMethod.PUT).build();
-				mapping.registerMapping(info, handler, putItemResourceMethod);
+				mapping.registerMapping(info, handler, putItemResourceMethod.get());
 			}
-			
-			Method patchItemResourceMethod = ReflectionUtils.findMethod(handlerClass, "patchItemResource",
-					new Class[] { domainType, domainIdType, PersistentEntityResourceAssembler.class, ETag.class, String.class });
 
-			if (patchItemResourceMethod != null
-					&& !patchItemResourceMethod.getDeclaringClass().equals(ResourceController.class)) {
-
+			Optional<Method> patchItemResourceMethod = selectMostSuitablePatchItemResourceMethod(handlerClass,
+					domainType, domainIdType);
+			if (isRedeclaredMethod(patchItemResourceMethod)) {
 				RequestMappingInfo info = RequestMappingInfo
 						.paths(new Path(repositoryRestConfiguration.getBasePath().toString()).slash(path.slash("{id"
 								+ (handler.getIdPathVariableRegex() != null ? ":" + handler.getIdPathVariableRegex()
 										: "")
 								+ "}")).toString())
 						.methods(RequestMethod.PATCH).build();
-				mapping.registerMapping(info, handler, patchItemResourceMethod);
+				mapping.registerMapping(info, handler, patchItemResourceMethod.get());
 			}
-			
-			Method deleteItemResourceMethod = ReflectionUtils.findMethod(handlerClass, "deleteItemResource",
-					new Class[] { domainIdType, ETag.class });
 
-			if (deleteItemResourceMethod != null
-					&& !deleteItemResourceMethod.getDeclaringClass().equals(ResourceController.class)) {
-
+			Optional<Method> deleteItemResourceMethod = selectMostSuitableDeleteItemResourceMethod(handlerClass,
+					domainIdType);
+			if (isRedeclaredMethod(deleteItemResourceMethod)) {
 				RequestMappingInfo info = RequestMappingInfo
 						.paths(new Path(repositoryRestConfiguration.getBasePath().toString()).slash(path.slash("{id"
 								+ (handler.getIdPathVariableRegex() != null ? ":" + handler.getIdPathVariableRegex()
 										: "")
 								+ "}")).toString())
 						.methods(RequestMethod.DELETE).build();
-				mapping.registerMapping(info, handler, deleteItemResourceMethod);
-			}
-			
-			
+				mapping.registerMapping(info, handler, deleteItemResourceMethod.get());
 
+			}
 		}
+	}
+
+	private static Optional<Method> selectMostSuitableGetCollectionResourceMethod(Class<?> clazz) {
+		return toStream(findMethod(clazz, GET_COLLECTION_RESOURCE,
+				new Class<?>[] { Pageable.class, PersistentEntityResourceAssembler.class }))//
+						.flatMap(it -> toStream(getMostSpecificMethod(it, clazz)))//
+						.findFirst();
+	}
+
+	private static Optional<Method> selectMostSuitableGetItemResourceMethod(Class<?> clazz, Class<?> domainIdType) {
+		Stream<Class<?>[]> source = Stream.of(//
+				new Class<?>[] { domainIdType, PersistentEntityResourceAssembler.class, HttpHeaders.class }, //
+				new Class<?>[] { Object.class, PersistentEntityResourceAssembler.class, HttpHeaders.class });
+		
+		return source//
+				.flatMap(it -> toStream(findMethod(clazz, GET_ITEM_RESOURCE, it)))//
+				.flatMap(it -> toStream(getMostSpecificMethod(it, clazz)))//
+				.findFirst();
+	}
+
+	private static Optional<Method> selectMostSuitablePostCollectionResourceMethod(Class<?> clazz,
+			Class<?> domainType) {
+
+		Stream<Class<?>[]> source = Stream.of(//
+				new Class<?>[] { domainType, PersistentEntityResourceAssembler.class, String.class }, //
+				new Class<?>[] { Object.class, PersistentEntityResourceAssembler.class, String.class });
+
+		return source//
+				.flatMap(it -> toStream(findMethod(clazz, POST_COLLECTION_RESOURCE, it)))//
+				.flatMap(it -> toStream(getMostSpecificMethod(it, clazz)))//
+				.findFirst();
+	}
+
+	private static Optional<Method> selectMostSuitablePutItemResourceMethod(Class<?> clazz, Class<?> domainType,
+			Class<?> domainIdType) {
+		Stream<Class<?>[]> source = Stream.of(//
+				new Class<?>[] { domainType, domainIdType, PersistentEntityResourceAssembler.class, ETag.class, String.class }, //
+				new Class<?>[] { Object.class, Object.class, PersistentEntityResourceAssembler.class, ETag.class, String.class });
+		
+		return source//
+				.flatMap(it -> toStream(findMethod(clazz, PUT_ITEM_RESOURCE, it)))//
+				.flatMap(it -> toStream(getMostSpecificMethod(it, clazz)))//
+				.findFirst();
+	}
+
+	private static Optional<Method> selectMostSuitablePatchItemResourceMethod(Class<?> clazz, Class<?> domainType,
+			Class<?> domainIdType) {
+		Stream<Class<?>[]> source = Stream.of(//
+				new Class<?>[] { domainType, domainIdType, PersistentEntityResourceAssembler.class, ETag.class, String.class }, //
+				new Class<?>[] { Object.class, Object.class, PersistentEntityResourceAssembler.class, ETag.class, String.class });
+		
+		return source//
+				.flatMap(it -> toStream(findMethod(clazz, PATCH_ITEM_RESOURCE, it)))//
+				.flatMap(it -> toStream(getMostSpecificMethod(it, clazz)))//
+				.findFirst();
+	}
+
+	private static Optional<Method> selectMostSuitableDeleteItemResourceMethod(Class<?> clazz, Class<?> domainIdType) {
+		Stream<Class<?>[]> source = Stream.of(//
+				new Class<?>[] { domainIdType, ETag.class }, //
+				new Class<?>[] { Object.class, ETag.class });
+		
+		return source//
+				.flatMap(it -> toStream(findMethod(clazz, DELETE_ITEM_RESOURCE, it)))//
+				.flatMap(it -> toStream(getMostSpecificMethod(it, clazz)))//
+				.findFirst();
+	}
+
+	/**
+	 * Looks up the most specific method for the given method and type and returns
+	 * an accessible version of discovered {@link Method} if found.
+	 *
+	 * @param method
+	 * @param type
+	 * @see ClassUtils#getMostSpecificMethod(Method, Class)
+	 * @return
+	 */
+	private static Optional<Method> getMostSpecificMethod(Method method, Class<?> type) {
+
+		return Optionals.toStream(Optional.ofNullable(ClassUtils.getMostSpecificMethod(method, type)))//
+				.map(it -> BridgeMethodResolver.findBridgedMethod(it))//
+				.peek(it -> ReflectionUtils.makeAccessible(it))//
+				.findFirst();
+	}
+
+	private static Optional<Method> findMethod(Class<?> type, String name, Class<?>... parameterTypes) {
+		return Optional.ofNullable(ReflectionUtils.findMethod(type, name, parameterTypes));
+	}
+
+	private static boolean isRedeclaredMethod(Optional<Method> method) {
+		return method.map(it -> !it.getDeclaringClass().equals(ResourceController.class)).orElse(false);
 	}
 
 	private static Class<?> resolveTypeParameter(List<TypeInformation<?>> arguments, int index,
